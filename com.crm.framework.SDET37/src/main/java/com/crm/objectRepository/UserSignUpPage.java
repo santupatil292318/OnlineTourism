575d9e8da614a05703aa14b56f127a933df0c673
santupatil <santupatil081@gmail.com>
2022-08-04 13:04:14 +0530

    update dashBoard

--- com.crm.framework.SDET37/src/main/java/com/crm/objectRepository/UserSignUpPage.java
+++ com.crm.framework.SDET37/src/main/java/com/crm/objectRepository/UserSignUpPage.java
@@ -2,7 +2,11 @@ package com.crm.objectRepository;
 
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
-
+/**
+ * 
+ * @author santu
+ *
+ */
 public class UserSignUpPage {
 	//declaration
 	@FindBy(name = "fname")
