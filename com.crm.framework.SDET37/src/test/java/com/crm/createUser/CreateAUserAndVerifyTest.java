17412ccdcd4119a074b441627ea68b3dd6d6ea40
shree <shree@DESKTOP-9BRV9HA>
2022-08-04 16:17:14 +0530

    update properties file

--- com.crm.framework.SDET37/src/test/java/com/crm/createUser/CreateAUserAndVerifyTest.java
+++ com.crm.framework.SDET37/src/test/java/com/crm/createUser/CreateAUserAndVerifyTest.java
@@ -1,14 +1,32 @@
 package com.crm.createUser;
 
 import com.crm.genericUtilities.BaseClass;
+import com.crm.objectRepository.HomePage;
+import com.crm.objectRepository.UserSignUpPage;
 
 public class CreateAUserAndVerifyTest extends BaseClass{
 	
 	public void createAUserAndVerifyTest() {
-		String newUSerName = eLib.readDataFromExcel("NewUser", 1, 0)+jLib.getRandomNumber();
-		String phNum = eLib.readDataFromExcel("NewUser", 1, 1);
+		
+		//fetch the newUserName from excel sheet
+		String newUserName = eLib.readDataFromExcel("NewUser", 1, 0)+jLib.getRandomNumber();
+		
+		//fetch the phone Number from excel sheet
+		String phoneNum = eLib.readDataFromExcel("NewUser", 1, 1);
+		
+		//fetch the emailID from excel sheet
 		String emailID = eLib.readDataFromExcel("NewUser", 1, 2)+jLib.getRandomNumber();
+		
+		//fetch the passWord from excel sheet
 		String passWord = eLib.readDataFromExcel("NewUser", 1, 2);
+		
+		//click on signup link
+		HomePage homePage = new HomePage(driver);
+		homePage.getSignUpLink().click();
+		
+		//enter user details
+		UserSignUpPage signUp = new UserSignUpPage(driver);
+		signUp.signUpToApp(newUserName, phoneNum, emailID, passWord);
 		
 		
 		
