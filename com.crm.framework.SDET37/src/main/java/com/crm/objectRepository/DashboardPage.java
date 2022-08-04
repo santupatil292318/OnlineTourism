9334b1eeca0182b8658f13bdcd4c085534cf19ad
santupatil <santupatil081@gmail.com>
2022-08-04 11:03:19 +0530

    dashboard

--- com.crm.framework.SDET37/src/main/java/com/crm/objectRepository/DashboardPage.java
+++ com.crm.framework.SDET37/src/main/java/com/crm/objectRepository/DashboardPage.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
 public class DashboardPage {
+	
 	//declaration
 	@FindBy(xpath = "//a[@class='sidebar-icon']")
 	private WebElement sideBarMenu;
